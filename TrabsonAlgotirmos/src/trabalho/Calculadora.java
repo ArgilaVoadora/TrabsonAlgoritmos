9393be06491cc1cd022a49e70ff0c7e1c28fe79e
Jadiel <38146469+JadielSantos@users.noreply.github.com>
2018-05-15 01:17:50 -0300

    15.05 Coloca Fila Infixada

--- TrabsonAlgotirmos/src/trabalho/Calculadora.java
+++ TrabsonAlgotirmos/src/trabalho/Calculadora.java
@@ -1,31 +1,74 @@
 package trabalho;
 
 public class Calculadora {
-
-    public static void main(String[] args) {
-        
+    public FilaVetor<String> extrairTermos(String expressao) {
+        expressao = expressao.replaceAll("\\s", "");
+        char[] termos = expressao.toCharArray();
+        int cont = 0;
+        FilaVetor<String> fila = new FilaVetor<>(contTermos(expressao));
+        for(int i = 0;i < fila.getLimite();i++){
+            String aux = "";        
+            for (int i2 = cont;i2 < termos.length;i2++) {
+                if (i2 == termos.length - 1) {
+                    aux += termos[i2] + "";
+                    break;
+                } else if (verificaTermos(termos[i2])){
+                    aux = termos[i2] + "";
+                    cont = i2 + 1;
+                    break;
+                } else if (verificaTermos(termos[i2 + 1])) { 
+                    aux += termos[i2] + "";
+                    cont = i2 + 1;
+                    break;
+                } else {
+                    aux += termos[i2];
+                }
+            }
+            fila.inserir(aux);
+        }
+        return fila;
     }
     
-    public String extrairTermos(String expressao){
-        //Tem que criar a filaA, so coloquei a string pra n dar erro
-        String filaA = "Bolsonaro2018";
-        String exp = "";
-        String exp2 = "";
+    public FilaVetor<String> gerarExprPosfixada(FilaVetor<String> exprInfixada) {
+        return null;
+    }
+    
+    public double calcularExprPosfixada(FilaVetor<String> exprPosfixada) {
+        return 0.0;
+    }
+    
+    public int contTermos(String expressao) {
+        int total = 0;
+        char[] termos = expressao.toCharArray();
         
-        exp = expressao;
-        
-        exp.replaceAll("[^0-9]", "!");
-        
-        for (int i = 0; i < expressao.length(); i++) {
-            if (!exp.substring(i, i).equals("!")) 
-                exp2 += exp.substring(i, i);
-            else{
-                filaA = exp2;
-                filaA = exp.substring(i, i);
+        for (int i = 0;i < termos.length;i++) {
+            if (verificaTermos(termos[i]))
+                total++;
+            else if (i != termos.length - 1) { 
+                if (verificaTermos(termos[i + 1])) {
+                    total++;
+                }
+            } else {
+                total++;
             }
         }
-
-        return filaA;
+        
+        return total;
     }
     
+    public boolean verificaTermos(char s) {
+        char[] termos = new char[6];
+        termos[0] = '+';
+        termos[1] = '-';
+        termos[2] = '*';
+        termos[3] = '/';
+        termos[4] = '(';
+        termos[5] = ')';
+        
+        for (int i = 0;i < termos.length;i++) {
+            if (s == termos[i])
+                return true;
+        }
+        return false;
+    }
 }
